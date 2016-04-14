bfa0a25fdbfa916d2fab958ba8aad245dd7408b8
Sergey Gernyak <sergeg1990@gmail.com>
2016-04-14 09:42:47 +0300

    Some cosmetic fixes after testing db

--- db_manager.sh
+++ db_manager.sh
@@ -7,11 +7,13 @@ export PG_CONTAINER_NAME='remark-pg'
 
 function pg_run_container {
   if [ ! -d "$DIRECTORY" ]; then
-    mkdir $PG_LOCAL_DATA
+    mkdir -p $PG_LOCAL_DATA
   fi
 
   docker run \
     --name $PG_CONTAINER_NAME \
+    -e POSTGRES_USER=admin \
+    -e POSTGRES_PASSWORD=password \
     -v $PG_LOCAL_DATA:/var/lib/postgresql/data \
     -d postgres:9.5.1
 }
