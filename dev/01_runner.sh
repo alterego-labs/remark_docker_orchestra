d135d42a1fe26e3c31496d177a93d6a5baadf4b3
Sergey Gernyak <sergeg1990@gmail.com>
2016-04-28 12:42:57 +0300

    Moved running web to the top

--- dev/01_runner.sh
+++ dev/01_runner.sh
@@ -7,18 +7,18 @@ source './api_manager.sh'
 source './web_manager.sh'
 source './nginx_manager.sh'
 
-echo "Runner. Step 1. Run PG container..."
+echo "=> Runner. Step 1. Run WEB container..."
+web_run_container;
+
+echo "=> Runner. Step 2. Run PG container..."
 pg_run_container;
 
-echo "Runner. Step 2. Run ecto migrations on API container..."
+echo "=> Runner. Step 3. Run ecto migrations on API container..."
 api_run_command "mix ecto.create";
 api_run_command "mix ecto.migrate";
 
-echo "Runner. Step 3. Run API container..."
+echo "=> Runner. Step 4. Run API container..."
 api_run_container;
 
-echo "Runner. Step 4. Run WEB container..."
-web_run_container;
-
-echo "Runnder. Step 5. Run NGINX container..."
+echo "=> Runner. Step 5. Run NGINX container..."
 nginx_run_container;
